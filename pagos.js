4b70b1026e15e99cacef0edd52222145cb271fce
Ariel <arielanus1980@yahoo.com.ar>
2022-05-05 00:40:43 -0300

    Subo archivos HTML y JavaScript

---
+++ pagos.js
@@ -0,0 +1,3 @@
+var grupoDePersonas= prompt("Ingrese el número")
+if (grupoDePersonas<8){alert("El pago total es de "+(grupoDePersonas*1.5))}
+else{alert("El pago total es de "+(grupoDePersonas*0.5))}
